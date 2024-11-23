527dda0a93482f00ca7f5be9162e2fdb0ebc7e21
Log_4n <jorgepalacios6422@gmail.com>
2024-11-23 17:57:52 -0300

    b

--- dager.js
+++ dager.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const container = document.querySelector('.model-container');
 
@@ -31,7 +31,7 @@ scene.add(directionalLight);
 let model;
 const loader = new GLTFLoader();
 loader.load(
-    './Media/pickling_cucumber/scene.gltf',
+    "./Media/pickling_cucumber/scene.gltf",
     function (gltf) {
         model = gltf.scene; // Acceso al modelo
         model.scale.set(35, 35, 35); // Ajustar el tamaño del modelo según sea necesario
